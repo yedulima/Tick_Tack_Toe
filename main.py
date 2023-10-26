ab827a6db3e04e86664029bc653daf0249fd5978
yedulima <eduardolimacmj2017@gmail.com>
2023-10-26 13:57:24 -0300

    FEAT / Completo

--- main.py
+++ main.py
@@ -10,21 +10,48 @@
 '''
 
 from modules import *
+import random
+import os
 
 if __name__ == '__main__':
     empty = ' '
     board = [
-        [[empty], [empty], [empty]],
-        [[empty], [empty], [empty]],
-        [[empty], [empty], [empty]]
+        [empty, empty, empty],
+        [empty, empty, empty],
+        [empty, empty, empty]
     ]
-    
-    while True:
-        showBoard(board)
-        op = int(input("se sabe: "))
-        verify, new_board = markInBoard(board, op, 'x')
 
-        print()
+    labels = 9
+
+    players = ['x', 'o']
+    player = random.choice(players)
+
+    os.system('clear')
+
+    while labels:
+        showBoard(board)
+        while True:
+            try:
+                op = int(input("Número de 1 a 9: "))
+            except:
+                print(f"\n{'⚠': ^42}\n╔{'─'*40}╗\n|{'Ação inválida':^40}{'|': ^2}\n╚{'─'*40}╝\n")
+                input()
+            else:
+                break
+
+        verify, new_board = markInBoard(board, op, player)
 
         if verify:
+            labels -= 1
             board = new_board
+            win_check = checkWinner(board, player)
+
+            if win_check:
+                showBoard(board)
+                break
+
+            player = swapPlayer(player, players)
+
+        os.system('clear')
+
+    print("Game over")
--- modules.py
+++ modules.py
@@ -2,38 +2,61 @@ def showBoard(board: list) -> None:
     for line in range(3):
         for column in range(3):
             if column != 2:
-                print(''.join(board[line][column]), end=' | ')
+                print(board[line][column], end=' | ')
                 continue
             print(''.join(board[line][column]))
         print('-'*10)
 
+def swapPlayer(current_player: str, players: list) -> str:
+    player_index = players.index(current_player)
+
+    if players[player_index] == players[-1]:
+        return players[0]
+    return players[-1]
+
 def returnNumberOfNumBoard(num: int) -> tuple:
     num_board = [
-        [[7], [8], [9]],
-        [[4], [5], [6]],
-        [[1], [2], [3]]
+        [7, 8, 9],
+        [4, 5, 6],
+        [1, 2, 3]
     ]
 
     for line in range(3):
         for column in range(3):
-            print(num_board[line][column][0], num_board[line][column][0] == num)
-            if num_board[line][column][0] == num:
+            if num_board[line][column] == num:
                 return line, column
     
-    return False, False # Not found
+    return 404, False # Not found
 
 def markInBoard(board: list, num: int, player: str) -> tuple:
     line, column = returnNumberOfNumBoard(num)
 
-    print(line, column)
+    if line == 404 or board[line][column] != ' ':
+        print(f"\n{'⚠': ^42}\n╔{'─'*40}╗\n|{'Ação inválida':^40}{'|': ^2}\n╚{'─'*40}╝\n")
+        input()
+        return False, '_'
 
-    if line == False:
-        print("Escolha inválida!")
-        return False, 'unknow'
-    
     board[line][column] = player
 
     return True, board
 
-def checkWinner(board):
-    pass
+def checkWinner(board: list, player: str) -> bool:
+    # Diagonal direita
+    if (board[0][2] == player) and (board[1][1] == player) and (board[2][0] == player):
+        return True
+
+    # Diagonar esquerda
+    elif (board[0][0] == player) and (board[1][1] == player) and (board[2][2] == player):
+        return True
+
+    # Horizontal
+    for line in range(3):
+        if (board[line][0] == player) and (board[line][1] == player) and (board[line][2] == player):
+            return True
+
+    # Vertical
+    for column in range(3):
+        if (board[0][column] == player) and (board[1][column] == player) and (board[2][column] == player):
+            return True
+
+    return False
